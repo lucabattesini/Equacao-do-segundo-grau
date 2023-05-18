654bbe919caaac1bedeec0dd9b9f2e1e8bf14f4e
Luca <lucabattesini1@gmail.com>
2023-05-18 11:53:48 -0300

    Delta without square root

--- main.py
+++ main.py
@@ -45,74 +45,80 @@ elif sidebar == "Equação do segundo grau":
 
     # DELTA
 
-    delta = b ** 2 - 4 * a * c
+    delta = int(b ** 2 - 4 * a * c)
+    deltaraiz = delta ** (1/2)
 
-    if a == 0:
+    if deltaraiz == int(delta):
+        if a == 0:
+            if st.button('Resposta'):
+                st.markdown(
+                    '''## É preciso que pelo menos o termo "A" seja preenchido para obter uma resposta''')
+
+            # DELTA MENOR QUE 0
+        elif delta < 0:
+
+            if st.button('Resposta'):
+                st.markdown('''## X não pertence a R''')
+                st.markdown(f'''## Delta é igual a: {delta}''')
+
+            # DELTA = 0
+        elif delta == 0:
+            x = int(-(b2) + delta**(1/2))
+            l = int(2 * a2)
+            s = x / l
+            if s == int(s):
+                if st.button('Resposta'):
+                    answer =  fr"\Large x = {int(s)}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+
+            else:
+
+                if st.button('Resposta'):
+                    answer = fr"\Large \frac{{{x}}}{l}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+
+            # DELTA MAIOR QUE 0
+        elif delta > 0:
+            radiciação = ((delta**(1/2)))
+
+            x1 = int(-1 * b2 + radiciação)
+            x2 = int(-1 * b2 - radiciação)
+
+            l1 = int(2 * a2)
+            l2 = l1
+
+            x1r = x1 / l1
+            x2r = x2 / l2
+
+            if x1r == int(x1r) and x2r == int(x2r):
+                if st.button('Resposta'):
+                    answer = fr"\Large x' = {int(x1r)} \newline x'' = {int(x2r)}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+
+            elif x1r == int(x1r) and x2r == float(x2r):
+                if st.button('Resposta'):
+                    answer = fr"\Large x' = {int(x1r)} \newline x'' = \frac{{{x2}}}{l2}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+
+            elif x1r == float(x1r) and x2r == int(x2r):
+                if st.button('Resposta'):
+                    answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = {int(x2r)}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+
+            elif x1r == float(x1r) and x2r == float(x2r):
+                if st.button('Resposta'):
+                    answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = \frac{{{x2}}}{l2}"
+                    st.latex(answer)
+                    st.markdown(f'''## Delta é igual a: {delta}''')
+    else:
         if st.button('Resposta'):
-            st.markdown(
-                '''## É preciso que pelo menos o termo "A" seja preenchido para obter uma resposta''')
-
-        # DELTA MENOR QUE 0
-    elif delta < 0:
-
-        if st.button('Resposta'):
-            st.markdown('''## X não pertence a R''')
-            st.markdown(f'''## Delta é igual a: {delta}''')
-
-        # DELTA = 0
-    elif delta == 0:
-        x = int(-(b2) + delta**(1/2))
-        l = int(2 * a2)
-        s = x / l
-        if s == int(s):
-            if st.button('Resposta'):
-                answer =  fr"\Large x = {int(s)}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
-
-        else:
-
-            if st.button('Resposta'):
-                answer = fr"\Large \frac{{{x}}}{l}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
-
-        # DELTA MAIOR QUE 0
-    elif delta > 0:
-        radiciação = ((delta**(1/2)))
-
-        x1 = int(-1 * b2 + radiciação)
-        x2 = int(-1 * b2 - radiciação)
-
-        l1 = int(2 * a2)
-        l2 = l1
-
-        x1r = x1 / l1
-        x2r = x2 / l2
-
-        if x1r == int(x1r) and x2r == int(x2r):
-            if st.button('Resposta'):
-                answer = fr"\Large x' = {int(x1r)} \newline x'' = {int(x2r)}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
-
-        elif x1r == int(x1r) and x2r == float(x2r):
-            if st.button('Resposta'):
-                answer = fr"\Large x' = {int(x1r)} \newline x'' = \frac{{{x2}}}{l2}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
-
-        elif x1r == float(x1r) and x2r == int(x2r):
-            if st.button('Resposta'):
-                answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = {int(x2r)}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
-
-        elif x1r == float(x1r) and x2r == float(x2r):
-            if st.button('Resposta'):
-                answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = \frac{{{x2}}}{l2}"
-                st.latex(answer)
-                st.markdown(f'''## Delta é igual a: {delta}''')
+                st.markdown('''## Delta não tem uma raiz Exata''')
+                st.markdown(f'''## Delta é igual a {delta}''')
 
 elif sidebar == "Conversão de temperaturas":
 
