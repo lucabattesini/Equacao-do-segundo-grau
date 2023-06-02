31a7cf3789cb8ff3ca8eb172896b2763cbbe10b3
Luca <lucabattesini1@gmail.com>
2023-06-02 10:49:25 -0300

    killing bugs

--- porcentagem.py
+++ porcentagem.py
@@ -7,29 +7,32 @@ def porcentagem():
     coluna1, coluna2 = st.columns(2)
 
     with coluna1:
-        total = st.number_input("Número total")
-        subtotal = st.number_input("Parte do número total")
+        total = st.number_input("Número total", value=0)
+        subtotal = st.number_input("Parte do número total", value=0)
     with coluna2:
         porcento = st.number_input("Porcentagem", value=100)
-        number = st.number_input("Porcentagem do número total")
-    
-    if number == 0:
-        subtotal2 = subtotal * porcento
-        s = subtotal2 / total
-        if st.button("resposta"):
-            st.markdown(f"## {subtotal} é igual a {s}% de {total}")
+        subporcento = st.number_input("Porcentagem do número total", value=0)
+
+    if subporcento == 0:
+        if total == 0:
+            st.warning(f"## Não é possivel dividir números por 0")
+        else:
+            subtotal2 = subtotal * porcento
+            s = subtotal2 / total
+            if st.button("resposta"):
+                st.markdown(f"## {subtotal} é igual a {s}% de {total}")
     
     elif subtotal == 0:
-        total2 = total * number
+        total2 = total * subporcento
         s = total2 / porcento
         if st.button("resposta"):
-            st.markdown(f"## {number}% de {total} é igual a {s}")
+            st.markdown(f"## {subporcento}% de {total} é igual a {s}")
     
     elif total == 0:
         subtotal2 = subtotal * porcento
-        s = subtotal2 / number
+        s = subtotal2 / subporcento
         if st.button("resposta"):
-            st.markdown(f"{subtotal} é igual a {number}% de {total}")
+            st.markdown(f"## {subtotal} é igual a {subporcento}% de {s}")
     
 
     
