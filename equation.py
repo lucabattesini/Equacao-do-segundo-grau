bf055294c7f48408e658a40c7334a646ee3484bd
Luca <lucabattesini1@gmail.com>
2023-06-06 22:37:24 -0300

    .

--- equation.py
+++ equation.py
@@ -42,78 +42,78 @@ def equacao():
         if st.button('Resposta'):
                 st.markdown('''## X não pertence a R''')
                 st.markdown(f'''## Delta é igual a: {delta}''')
+    elif delta > 0:            
+        if deltaraiz == int(deltaraiz):
+            if a == 0:
+                if st.button('Resposta'):
+                    st.markdown(
+                        '''## É preciso que pelo menos o termo "A" seja preenchido para obter uma resposta''')
 
-    elif deltaraiz == int(deltaraiz):
-        if a == 0:
+                # DELTA = 0
+            if delta == 0:
+                x = int(-(b2) + delta**(1/2))
+                l = int(2 * a2)
+                s = x / l
+                if s == int(s):
+                    if st.button('Resposta'):
+                        answer =  fr"\Large x = {int(s)}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+
+                else:
+
+                    if st.button('Resposta'):
+                        answer = fr"\Large \frac{{{x}}}{l}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+
+                # DELTA MAIOR QUE 0
+            elif delta > 0:
+                radiciação = ((delta**(1/2)))
+
+                x1 = int(-1 * b2 + radiciação)
+                x2 = int(-1 * b2 - radiciação)
+
+                l1 = int(2 * a2)
+                l2 = l1
+
+                x1r = x1 / l1
+                x2r = x2 / l2
+
+                if x1r == int(x1r) and x2r == int(x2r):
+                    if st.button('Resposta'):
+                        answer = fr"\Large x' = {int(x1r)} \newline x'' = {int(x2r)}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+
+                elif x1r == int(x1r) and x2r == float(x2r):
+                    if st.button('Resposta'):
+                        answer = fr"\Large x' = {int(x1r)} \newline x'' = \frac{{{x2}}}{l2}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+
+                elif x1r == float(x1r) and x2r == int(x2r):
+                    if st.button('Resposta'):
+                        answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = {int(x2r)}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+
+                elif x1r == float(x1r) and x2r == float(x2r):
+                    if st.button('Resposta'):
+                        answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = \frac{{{x2}}}{l2}"
+                        st.latex(answer)
+                        answerd = fr'\Large Δ = {delta}'
+                        st.latex(answerd)
+        else:
             if st.button('Resposta'):
-                st.markdown(
-                    '''## É preciso que pelo menos o termo "A" seja preenchido para obter uma resposta''')
-
-            # DELTA = 0
-        if delta == 0:
-            x = int(-(b2) + delta**(1/2))
-            l = int(2 * a2)
-            s = x / l
-            if s == int(s):
-                if st.button('Resposta'):
-                    answer =  fr"\Large x = {int(s)}"
-                    st.latex(answer)
+                    st.markdown('''## Delta não tem uma raiz Exata''')
                     answerd = fr'\Large Δ = {delta}'
                     st.latex(answerd)
-
-            else:
-
-                if st.button('Resposta'):
-                    answer = fr"\Large \frac{{{x}}}{l}"
-                    st.latex(answer)
-                    answerd = fr'\Large Δ = {delta}'
-                    st.latex(answerd)
-
-            # DELTA MAIOR QUE 0
-        elif delta > 0:
-            radiciação = ((delta**(1/2)))
-
-            x1 = int(-1 * b2 + radiciação)
-            x2 = int(-1 * b2 - radiciação)
-
-            l1 = int(2 * a2)
-            l2 = l1
-
-            x1r = x1 / l1
-            x2r = x2 / l2
-
-            if x1r == int(x1r) and x2r == int(x2r):
-                if st.button('Resposta'):
-                    answer = fr"\Large x' = {int(x1r)} \newline x'' = {int(x2r)}"
-                    st.latex(answer)
-                    answerd = fr'\Large Δ = {delta}'
-                    st.latex(answerd)
-
-            elif x1r == int(x1r) and x2r == float(x2r):
-                if st.button('Resposta'):
-                    answer = fr"\Large x' = {int(x1r)} \newline x'' = \frac{{{x2}}}{l2}"
-                    st.latex(answer)
-                    answerd = fr'\Large Δ = {delta}'
-                    st.latex(answerd)
-
-            elif x1r == float(x1r) and x2r == int(x2r):
-                if st.button('Resposta'):
-                    answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = {int(x2r)}"
-                    st.latex(answer)
-                    answerd = fr'\Large Δ = {delta}'
-                    st.latex(answerd)
-
-            elif x1r == float(x1r) and x2r == float(x2r):
-                if st.button('Resposta'):
-                    answer = fr"\Large x' = \frac{{{x1}}}{l1} \newline x'' = \frac{{{x2}}}{l2}"
-                    st.latex(answer)
-                    answerd = fr'\Large Δ = {delta}'
-                    st.latex(answerd)
-    else:
-        if st.button('Resposta'):
-                st.markdown('''## Delta não tem uma raiz Exata''')
-                answerd = fr'\Large Δ = {delta}'
-                st.latex(answerd)
     # DEMONSTRAÇÃO DO CÓDIGO
 
     st.divider()
