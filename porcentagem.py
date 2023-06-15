43f9337c16913530bfbfed544bc75aee7ba0cfe5
Luca <lucabattesini1@gmail.com>
2023-06-15 17:46:38 -0300

    adding percentage again

--- porcentagem.py
+++ porcentagem.py
@@ -14,27 +14,35 @@ def porcentagem():
     with xy:
         coluna, coluna1 = st.columns(2)
         with coluna:
-            a = st.number_input("X", value = 0, key="k1")
+            a = st.number_input("X", value = 1, key="k1")
         with coluna1:
-            b = st.number_input("Y", value= 0, key="k2")
+            b = st.number_input("Y", value= 1, key="k2")
+        x = a * 100
+        r = x / b
         if st.button("Resposta", key="resposta1"):
-            st.markdown("Resposta")
+            st.markdown(f"{a} é igual a {r}% de {b}")
+
     with xy2:
         coluna2, coluna3 = st.columns(2)
         with coluna2:
-            c = st.number_input("X", value= 0, key="k3")
+            c = st.number_input("X", value= 1, key="k3")
         with coluna3:
-            d = st.number_input("Y", value= 0, key="k4")
+            d = st.number_input("Y", value= 1, key="k4")
+        x1 = c * d
+        r1 = x1 / 100
         if st.button("Resposta", key="resposta2"):
-            st.markdown("Resposta")
+            st.markdown(f"{c}% de {d} é igual a {r1}")
+
     with xy3:
         coluna4, coluna5 = st.columns(2)
         with coluna4:
-            e = st.number_input("X", value= 0, key="k5")
+            e = st.number_input("X", value= 1, key="k5")
         with coluna5:
-            f = st.number_input("Y", value= 0, key="k6")
+            f = st.number_input("Y", value= 1, key="k6")
+        x2 = e * 100
+        r2 = x2 / f
         if st.button("Resposta", key="resposta3"):
-            st.markdown("Resposta")
+            st.markdown(f"{e} é igual a {f}% de {r2}")
         
 
     
