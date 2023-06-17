4ddeaeb118af5c3f9d2264388edf4ccb1ce43290
Luca <lucabattesini1@gmail.com>
2023-06-17 00:13:34 -0300

    removing the about button

--- menu.py
+++ menu.py
@@ -6,7 +6,5 @@ def menu():
     st.markdown("# Bem vindo ao Stayline")
     st.markdown("## Calculadora de funções")
 
-    if st.button("Sobre"):
-        st.markdown("#### Este site foi desenvouvido como objetivo principal o acadêmico de auxiliar o aprendizado e incentivar o estudo e uso da programação. O web-site stayline pode ser utilizado de diversas formas, basta inserir os números no lugar indicado e apertar no botão “resposta”. Inicialmente o site é dividido em quatro partes, o menu principal e suas atuais 3 funções que consistem em termologia, resolução de equações do 2º grau e calculos com porcentagem. Basta apertar na barra lateral no canto esquedo da tela e selecionar a função desejada, tem algo mais facil que isso? Na determinada função que escolha terá um botão escrito “Mostrar código” onde ao apertalo aparecerá todo o codigo escrito para o desenvolvimento de tal função, reforçando o incentivo do estudo da programação.")
     st.divider()
     st.markdown('''**Desenvolvido por Luca Battesini**''')
